b98bf1f8634a01dc1521b60984e471ecd20dffc6
Patrick Mollohan <patrickmollohan@yahoo.com>
2015-10-15 01:06:08 -0400

    GUI now opens in centre of screen

--- src/password/GUI.java
+++ src/password/GUI.java
@@ -55,7 +55,8 @@ public class GUI extends JFrame {
 		// Frame properties
 		setTitle("Random Password Generator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, 450, 300);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
